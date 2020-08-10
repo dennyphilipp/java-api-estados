fda364df03586ccde6d354433ff8760aa15e8700
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2020-08-10 19:09:48 -0300

    Implementado teste em EstadoServico

--- src/test/java/com/github/dennyphilipp/api/ApiApplicationTests.java
+++ src/test/java/com/github/dennyphilipp/api/ApiApplicationTests.java
@@ -1,9 +1,14 @@
 package com.github.dennyphilipp.api;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApiApplicationTests.class)
+@ActiveProfiles("teste")
 class ApiApplicationTests {
 
 	@Test
---
+++ src/test/java/com/github/dennyphilipp/api/servico/EstadoServicoTeste.java
@@ -0,0 +1,46 @@
+package com.github.dennyphilipp.api.servico;
+
+import com.github.dennyphilipp.dto.EstadoDTO;
+import com.github.dennyphilipp.servico.EstadoService;
+import com.github.dennyphilipp.terceiro.IIBGEEstado;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("teste")
+public class EstadoServicoTeste {
+    @Autowired
+    EstadoService estadoService;
+
+    @Mock
+    IIBGEEstado ibgeEstado;
+
+    @Test
+    public void injetarDependenciaSucesso()  {
+        assertThat(ibgeEstado).isNotNull();
+    }
+
+    @Test
+    public void injetarDependenciaIBGESucesso()  {
+        assertThat(ibgeEstado).isNotNull();
+    }
+
+    @Test
+    public  void  obterTodosSucesso() {
+        List<EstadoDTO> estados = this.estadoService.obterTodos();
+
+        assertThat(estados).isNotNull();
+        assertThat(estados).isNotEmpty();
+        assertThat(estados.size()).isGreaterThan(0);
+    }
+}
